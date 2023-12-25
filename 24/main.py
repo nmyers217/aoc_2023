0be5d34b5e695a3ee871b1991c842ca6c3d26515
nmyers217 <nickbmyers217@gmail.com>
2023-12-24 21:15:12 -0600

    Replace day 24 part 1 math with easier parametric solution

--- 24/main.py
+++ 24/main.py
@@ -10,36 +10,58 @@ data = parse_input()
 MIN = 200000000000000
 MAX = 400000000000000
 
-def p1():
-    lines = []
-    for p,v in data:
-        # Remember, y = mx + b -- m is slope, and b is intercept
-        (x,y,_) = p
-        (dx,dy,_) = v
-        m = dy / dx # Slope is rise / run = deltaY / deltaX
-        b = y - (m * x)
-        lines.append([m, b, x, dx > 0])
+def ray_intersect(ray_a, ray_b):
+    """
+    Parametric solution to ray intersection
 
+    A ray is some point P and a vec V, where V can be scaled b U to represent any point on the ray
+
+    Two rays A and B intersect where a solution can be found in the following system of equations:
+    
+    A + Va*u = B + Vb*v
+    """
+    # point 1, vec 1, point 2, vec 2
+    (p1,v1), (p2,v2) = ray_a, ray_b
+    # A and Delta A vecs
+    (ax,ay), (adx,ady) = p1[:2], v1[:2]
+    # B and Delta B vecs
+    (bx,by), (bdx,bdy) = p2[:2], v2[:2]
+
+    # Delta from b-a
+    dx,dy = bx-ax, by-ay
+
+    # Determinant
+    det = bdx*ady - bdy*adx
+
+    if det == 0:
+        # The rays are parallel
+        return None
+
+    # U and V scalars to determine point of intersection
+    u = (dy*bdx - dx*bdy) / det
+    v = (dy*adx - dx*ady) / det
+
+    if u <= 0 or v <= 0:
+        # Means intersection is behind ray (in the past)
+        return None
+
+    # Intersection locations (equal within epsilon due to floating point precision)
+    ix, iy = ax + adx*u, ay + ady*u
+
+    return ix, iy
+
+def p1():
     res = 0
     seen = set()
-    for i in range(len(lines)):
-        for j in range(len(lines)):
+    for i in range(len(data)):
+        for j in range(len(data)):
             if i == j or (j,i) in seen:
                 continue
             seen.add((i,j))
-            m1,b1,x1,dx1 = lines[i]
-            m2,b2,x2,dx2 = lines[j]
-
-            if m1 == m2:
-                # parallel
+            loc = ray_intersect(data[i], data[j])
+            if loc is None:
                 continue
-            xintercept = (b2 - b1) / (m1 - m2)
-            if (xintercept < x1) if dx1 else (xintercept > x1):
-                continue
-            if (xintercept < x2) if dx2 else (xintercept > x2):
-                continue
-            yintercept = (m1 * xintercept) + b1
-            if all(MIN <= v <= MAX for v in (xintercept, yintercept)):
+            if all(MIN <= v <= MAX for v in loc):
                 res += 1
 
     return res
